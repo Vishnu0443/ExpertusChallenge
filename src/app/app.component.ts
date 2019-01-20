27258ef02db04743b9a988cb08153708a2f72b23
Vishnu0443 <46652062+Vishnu0443@users.noreply.github.com>
2019-01-20 00:43:02 -0500

    UI Changes.
    Client side changes. Calling Public API's, Redirecting the links to Expertus website, Displayed Company profile.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm, FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ExpertusChallengeAccepted';
+export class AppComponent implements OnInit {
+  messageForm: FormGroup;
+  title = 'Hello Expertus!';
+  ClientInfo: any;
+  weatherInfo: any;
+  CurrentLocation: string;
+  Temp: number;
+  pressure: number;
+  Humidity: number;
+  constructor(private _fb: FormBuilder, private _http: HttpClient){
+    this.messageForm = this._fb.group({
+      _firstName:['',[Validators.required]],
+      _lastName:['',[Validators.required]],
+      _emailAddress:['',[Validators.required]],
+      _phoneNumber:['',[Validators.required]],
+      _textmessage: ['',[Validators.required]]
+    })
+  }
+  ngOnInit(){
+      this.GetClientInfo().subscribe((data:any)=>{
+        if(data != null || data!= undefined){
+          this.ClientInfo = data;
+          this.CurrentLocation = data.city;
+          this.GetWeatherInfo(data.country_code).subscribe((data:any)=>{
+            if(data != null || data!= undefined){
+              this.weatherInfo = data;
+              this.Temp = data.main.temp;
+              this.pressure = data.main.pressure;
+              this.Humidity= data.main.humidity;
+            }
+          })
+        }        
+      })
+      
+  }
+  GetClientInfo(){
+   var Info = this.ClientInfo =  this._http.get('http://api.ipstack.com/check?access_key=afe57d07d71520250475e0329195df5c');
+   return Info;
+  }
+  GetWeatherInfo(countrycode){
+    var url = 'https://api.openweathermap.org/data/2.5/weather?q='+this.CurrentLocation +','+countrycode+'&appid=439cff8b1553fdf182a8c4f478e8e554';
+    var WInfo  = this.weatherInfo = this._http.get(url);
+    return WInfo;
+  }
+  SendMessage(){
+    this.messageForm.value._firstName
+    this.messageForm.value._lastName
+    this.messageForm.value._phoneNumber
+    this.messageForm.value._emailAddress
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+//Framework Components
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+//User Defined Components
+const appRoutes: Routes =[
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes), FormsModule,ReactiveFormsModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
